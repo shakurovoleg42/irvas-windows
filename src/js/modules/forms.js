1d921161a6ec180a19c935531ade1f9f305d3a0b
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-05-01 15:03:16 +0500

    modified:   src/index.html 	modified:   src/js/main.js 	new file:   src/js/modules/changeModalState.js 	new file:   src/js/modules/checkNumInputs.js 	modified:   src/js/modules/forms.js 	modified:   src/js/modules/modals.js 	new file:   src/js/modules/timer.js

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,21 +1,11 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
     const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input'),
-          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
-        //   nameInputs = document.querySelectorAll('input[name="user_name"]');
+          inputs = document.querySelectorAll('input');
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
-
-    // nameInputs.forEach(item => {
-    //     item.addEventListener('input', () => {
-    //         item.value = item.value.replace(/\W/, '');
-    //     });
-    // });
-
+    checkNumInputs('input[name="user_phone"]');
+    
     const message = {
         loading: 'Загрузка...',
         success: 'Спасибо! Скоро мы с вами свяжемся',
@@ -47,6 +37,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
